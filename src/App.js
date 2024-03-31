2e2f4da309f666f9e9a057d78d4c9a01a645950d
AJIT SINGH <sajit3541@gmail.com>
2024-03-31 21:21:39 +0530

    responsive design

--- src/App.js
+++ src/App.js
@@ -1,34 +1,81 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Map from "./Components/Content/Map";
+import close from "./assets/images/closeicon.png"
 import { useState, useEffect } from "react";
 function App() {
   const [currentComponent, setCurrentComponent] = useState({
-    "Home": true,
-    "Parties": false,
-    "Dates": false,
-    "Info": false
+    Home: true,
+    Parties: false,
+    Dates: false,
+    Info: false,
   });
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   useEffect(() => {
     console.log("Current Component 1", currentComponent);
-  }, [currentComponent, ]);
+  }, [currentComponent]);
 
   const handleNavigation = (value) => {
     const updatedComponent = Object.fromEntries(
-      Object.keys(currentComponent).map(key => [key, key === value])
+      Object.keys(currentComponent).map((key) => [key, key === value])
     );
     setCurrentComponent(updatedComponent);
     console.log("Current Component", currentComponent);
-  }
+  };
 
   return (
-    <div className="parent">
-      <Navbar handleNavigation={handleNavigation} />
-      <div className="container-fluid">
-        <Map currentComponent={currentComponent} />
+    <>
+      {mobileMenu && (
+        <div className={`mobile-drawer ${mobileMenu ? "open" : ""}`}>
+          <span
+            className="menu-icon"
+            onClick={() => setMobileMenu(!mobileMenu)}
+          >
+            <img src={close} alt="menu" height={20} width={20} />
+          </span>
+          <br />
+          {mobileMenu && (
+            <ul className="mini-navlink">
+              <li
+                className="mini-navlink-item"
+                onClick={() => handleNavigation("Home")}
+              >
+                Home
+              </li>
+              <li
+                className="mini-navlink-item"
+                onClick={() => handleNavigation("Parties")}
+              >
+                Political Parties
+              </li>
+              <li
+                className="mini-navlink-item"
+                onClick={() => handleNavigation("Dates")}
+              >
+                Election Dates
+              </li>
+              <li
+                className="mini-navlink-item"
+                onClick={() => handleNavigation("Info")}
+              >
+                Voter's Guide
+              </li>
+            </ul>
+          )}
+        </div>
+      )}
+      <div className="parent">
+        <Navbar
+          handleNavigation={handleNavigation}
+          toggleMobileMenu={setMobileMenu}
+          mobileMenu={mobileMenu}
+        />
+        <div className="container-fluid">
+          <Map currentComponent={currentComponent} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,45 @@
 import React, { useState } from "react";
-// import { NavLink } from "react-router-dom";
 import "../../App.css";
+import menu from "../../assets/images/menuicon.png"
 import "./Navbar.css";
 
-const Navbar = ({ handleNavigation }) => {
+const Navbar = ({ handleNavigation, toggleMobileMenu, mobileMenu }) => {
+  // const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
-    <nav className="navbar">
-      <span className="navbar-brand mb-0 h1" onClick={() => handleNavigation('Home')}>Election Hub</span>
-      <ul className="navlink">
-      <li className="navlink-item" onClick={() => handleNavigation('Home')} >Home</li>
-        <li className="navlink-item" onClick={() => handleNavigation('Parties')}>Political Parties</li>
-        <li className="navlink-item" onClick={() => handleNavigation('Dates')}>Election Dates</li>
-        <li className="navlink-item" onClick={() => handleNavigation('Info')}>Voter's Guide</li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <span
+          className="navbar-brand mb-0 h1"
+          onClick={() => handleNavigation("Home")}
+        >
+          Election Hub
+        </span>
+        <ul className="navlink">
+          <li className="navlink-item" onClick={() => handleNavigation("Home")}>
+            Home
+          </li>
+          <li
+            className="navlink-item"
+            onClick={() => handleNavigation("Parties")}
+          >
+            Political Parties
+          </li>
+          <li
+            className="navlink-item"
+            onClick={() => handleNavigation("Dates")}
+          >
+            Election Dates
+          </li>
+          <li className="navlink-item" onClick={() => handleNavigation("Info")}>
+            Voter's Guide
+          </li>
+        </ul>
+        <span className="mobile-menu" onClick={() => toggleMobileMenu(!mobileMenu)}>
+          <img src={menu} alt="menu" height={20} width={20} />
+        </span>
+      </nav>
+    </>
   );
 };
 
